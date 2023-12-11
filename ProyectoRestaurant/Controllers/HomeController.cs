5d0a1b4276664b9c5f43b7d5440423fcbd7de3a6
SantiVentu <santyventu@gmail.com>
2023-12-11 18:11:55 -0300

    aca cree varios controladores , crearMenu get y post, delete menu y confirm delete

--- ProyectoRestaurant/Controllers/HomeController.cs
+++ ProyectoRestaurant/Controllers/HomeController.cs
@@ -60,5 +60,67 @@ namespace ProyectoRestaurant.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        [HttpGet]
+        public IActionResult CrearMenu()
+        {
+            return View(new Menu()); //nos da un nuevo menu en lugar de una lista 
+        }
+
+        [HttpPost]
+        public IActionResult CrearMenu(Menu nuevoMenu)
+        {
+            if (ModelState.IsValid)
+            {
+                // Lógica para agregar el nuevo menú a la base de datos
+                _context.Menus.Add(nuevoMenu);
+                _context.SaveChanges();
+
+                TempData["SuccessMessage"] = "Menú agregado con éxito";
+
+                // Redirige de nuevo a la vista "Menu" con el nuevo menú agregado
+                return RedirectToAction("Menu");
+            }
+
+            // Si el modelo no es válido, vuelve a la vista de agregar menú para corregir los errores
+            return View(nuevoMenu);
+        }
+
+
+
+        public IActionResult DeleteMenu(int id)
+        {
+            // Fetch the menu item from the database based on the ID
+            var menu = _context.Menus.Find(id);
+
+            if (menu == null)
+            {
+                // Handle the case where the menu item is not found
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+
+        [HttpPost]
+        public IActionResult ConfirmDelete(int id)
+        {
+            var menu = _context.Menus.Find(id);
+
+            if (menu == null)
+            {
+                // Handle the case where the menu item is not found
+                return NotFound();
+            }
+
+            _context.Menus.Remove(menu);
+            _context.SaveChanges();
+
+            TempData["SuccessMessage"] = "El menu fue eliminado correctamente";
+
+            return RedirectToAction("Menu");
+        }
     }
 }
